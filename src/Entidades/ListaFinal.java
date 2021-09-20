f153a9bfa94d5e25ba7c3e8078358420b2fe63b7
DAW209 <gillerav01@educantabria.es>
2021-09-20 19:33:17 +0200

    Primer commit

---
+++ src/Entidades/ListaFinal.java
@@ -0,0 +1,8 @@
+package Entidades;
+
+public class ListaFinal {
+    private Nodo prim;
+    private Nodo fin;
+    
+    /* TODO Constructores, get, set, add, addFin, mostrar, BorrarPrimero, Borrar*/
+}
---
+++ src/Entidades/Nodo.java
@@ -0,0 +1,32 @@
+package Entidades;
+
+public class Nodo {
+    private int info;
+    private Nodo sig;
+
+    public Nodo() {
+        this.info = 0;
+        this.sig = null;
+    }
+
+    public Nodo(int info, Nodo sig) {
+        this.info = info;
+        this.sig = sig;
+    }
+
+    public int getInfo() {
+        return info;
+    }
+
+    public void setInfo(int info) {
+        this.info = info;
+    }
+
+    public Nodo getSig() {
+        return sig;
+    }
+
+    public void setSig(Nodo sig) {
+        this.sig = sig;
+    }
+}
