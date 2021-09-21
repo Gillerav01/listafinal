2b6f0a9f68dcf1751d11febc15304e6b0824a782
Guillermo <gillerav01@educantabria.es>
2021-09-21 11:40:19 +0200

    Segundo commit

--- src/Entidades/ListaFinal.java
+++ src/Entidades/ListaFinal.java
@@ -4,5 +4,88 @@ public class ListaFinal {
     private Nodo prim;
     private Nodo fin;
     
-    /* TODO Constructores, get, set, add, addFin, mostrar, BorrarPrimero, Borrar*/
+    /* TODO BorrarPrimero, Borrar*/
+
+    public ListaFinal() {
+        this.prim = null;
+        this.fin = null;
+    }
+
+    public Nodo getPrim() {
+        return prim;
+    }
+
+    public void setPrim(Nodo prim) {
+        this.prim = prim;
+    }
+
+    public Nodo getFin() {
+        return fin;
+    }
+
+    public void setFin(Nodo fin) {
+        this.fin = fin;
+    }
+    
+    public boolean esVacia() {
+        return this.prim == null && this.fin == null;
+    }
+    
+    public void add (int x){
+        Nodo nuevo = new Nodo(x, null);
+        
+        if (this.esVacia()) {
+            prim = nuevo;
+            fin = nuevo;
+        } else {
+            fin = prim;
+            nuevo.setSig(prim);
+            prim = nuevo;
+        }
+    }
+    
+    public void addFin (int x){
+        Nodo nuevo = new Nodo(x, null);
+        if (this.esVacia()) {
+            prim = nuevo;
+            fin = nuevo;
+        } else {
+            fin.setSig(nuevo);
+            fin = nuevo;
+        }
+    }
+    
+    public void leer () {
+        Nodo aux = prim;
+
+        while (aux != null) {
+            System.out.println(aux.getInfo());
+            aux = aux.getSig();
+        }
+    }
+    
+    public boolean borrar (int x) {
+        boolean encontrado = false;
+        Nodo aux = prim;
+        
+            while (aux.getSig() != null) {
+                
+                if ( aux.getInfo() == x){
+                    encontrado = true;
+                }
+                aux = aux.getSig();
+            }
+        
+        return encontrado;
+    }
+    
+    public boolean borrarPrimero () {
+        boolean encontrado = false;
+        
+            
+        
+        return encontrado;
+    }
+    
+    
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,15 @@
 package Main;
 
+import Entidades.ListaFinal;
+
 public class Main {
 
     public static void main(String[] args) {
+        ListaFinal l1 = new ListaFinal ();
         
+        l1.add(5);
+        l1.addFin(7);
+        l1.leer();
     }
     
 }
